0748b029ddbb5a6673063b9e1d85dca6c8fb1e6b
rashid-inkess <inkessrashid@gmail.com>
2025-12-31 14:54:50 +0100

    chore: add backend CI with GitHub Actions (#2)
    * chore: add GitHub Actions CI for backend
    
    * feat: add minimal Spring Boot backend skeleton

---
+++ backend/src/main/java/com/devflow/tracker/DevflowTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.devflow.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevflowTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevflowTrackerApplication.class, args);
+    }
+}
